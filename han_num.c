f0d3975014c376fc511a900297c029e3a248ffe8
injeChoi <2015jasonc@gmail.com>
2019-06-03 15:39:34 +0900

    한수 계산기

--- han_num.c
+++ han_num.c
@@ -2,33 +2,26 @@
 
 int main(void)
 {
-  int i,j,k,t,a[3];
+  int a, b, c, cnt;
   unsigned int N;
-  int han = 0;
   scanf("%d", &N);
 
-  for(i=1;i<=N;i++)
+  if(N<100)
+    printf("%d\n", N);
+  else
   {
-    if(i<100)
-      han = i;
-
-    else if(i == 1000)
-      break;
-
-    else
+    cnt = 99;
+    for(int i=100;i<=N;i++)
     {
-      k=0; t=1;
-      while(t>0)
-      {
-        a[k] = t % 10;
-        t /= 10;
-        k++;
-      }
-      if(a[0] - a[1] == a[1] - a[2])
-        han++;
+      a = i / 100; //백의 자리
+      b = (i/10) % 10; // 십의 자리
+      c = i % 10; // 일의 자리
+
+      if(a-b == b-c)
+        cnt++;
     }
+    printf("%d\n", cnt);
   }
-  printf("%d\n", han);
 
   return 0;
 }
