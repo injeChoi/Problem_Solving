b8469d7a6657d194f9ee97a3554181a2ea483990
injeChoi <2015jasonc@gmail.com>
2019-09-18 00:25:34 +0900

    백준 9461번, 파도반 수열

---
+++ BOJ_9461_파도반수열.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  int T, N;
+  long long P[101]; // 일정 수 이상이면 기하급수 적으로 크기 증가
+  cin >> T;
+
+  P[1] = 1;
+  P[2] = 1;
+  P[3] = 1;
+  P[4] = 2;
+  P[5] = 2;
+
+  for (int i = 0; i < T; i++) {
+    cin >> N;
+    if (1 <= N &&N <= 100) {
+      for (int j = 6; j <= N; j++)
+        P[j] = P[j-1] + P[j-5];
+      cout << P[N] << endl;
+    }
+  }
+
+  return 0;
+}
