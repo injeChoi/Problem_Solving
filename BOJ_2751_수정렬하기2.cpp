dac4d78bd6bdddf0b19d840b253b01b58bf66586
injeChoi <2015jasonc@gmail.com>
2020-01-18 16:47:01 +0900

    백준 2751번, 수 정렬하기2

---
+++ BOJ_2751_수정렬하기2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  int N;
+  int num;
+  cin >> N;
+
+  vector<int> arr;
+
+  for (int i = 0; i < N; i++) {
+    cin >> num;
+    arr.push_back(num);
+  }
+
+  sort(arr.begin(), arr.end());
+
+  for (int i = 0; i < N; i++) {
+    cout << arr[i] << "\n";
+  }
+
+  return 0;
+}
