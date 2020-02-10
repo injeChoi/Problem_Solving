1cf893040f8e4e23762f5433eff1e1605624269b
injeChoi <2015jasonc@gmail.com>
2020-02-10 21:37:15 +0900

    백준 1149번, RGB거리

--- BOJ_1149_RGB거리.cpp
+++ BOJ_1149_RGB거리.cpp
@@ -1,28 +1,28 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-  int N;
-  int temp;
-  int dp[1001][3] = { 0 }; // dp[ ][0] = Red, dp[ ][1] = Green, dp[ ][2] = Blue
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
 
-  cin >> N;
+  int N; cin >> N;
+  vector<vector<int> > dp(N, vector<int>(3));
 
-  for (int i = 1; i <= N; i++)
+  for (int i = 0; i < N; i++)
     cin >> dp[i][0] >> dp[i][1] >> dp[i][2];
 
-
-  for (int i = 1; i <= N; i++) {
+  for (int i = 1; i < N; i++) {
     dp[i][0] += min(dp[i-1][1], dp[i-1][2]);
     dp[i][1] += min(dp[i-1][0], dp[i-1][2]);
     dp[i][2] += min(dp[i-1][0], dp[i-1][1]);
   }
 
-  temp = min(dp[N][0], dp[N][1]);
-  temp = min(temp, dp[N][2]);
+  int temp = min(dp[N-1][0], dp[N-1][1]);
+  temp = min(temp, dp[N-1][2]);
 
-  cout << temp << endl;
-
+  cout << temp << "\n";
   return 0;
 }
