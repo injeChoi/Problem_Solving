505b04bbc394f8b45a0157c35a1f96b92af0640b
injeChoi <2015jasonc@gmail.com>
2020-02-10 15:32:36 +0900

    백준 1463번, 1로 만들기 (vector)

--- BOJ_1463_1로만들기.cpp
+++ BOJ_1463_1로만들기.cpp
@@ -1,28 +1,27 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 using namespace std;
 
-int dp[10000001];
-
 int main(int argc, char const *argv[]) {
-  int N, res = 0;
-  cin >> N;
+  int N; cin >> N;
+  int ans;
+  vector<int> v(1000001);
 
-  dp[1] = 0;
+  v[1] = 0;
   for (int i = 2; i <= N; i++) {
-    if(i%3 == 0)
-      dp[i] = min(dp[i-1] + 1, dp[i/3] + 1);
+    v[i] = v[i-1] + 1;
 
-    else if(i%2 == 0)
-      dp[i] = min(dp[i-1] + 1, dp[i/2] + 1);
+    if(i % 3 == 0)
+      v[i] = min(v[i], v[i/3]+1);
 
-    else
-      dp[i] = dp[i-1] + 1;
+    if(i % 2 == 0)
+      v[i] = min(v[i], v[i/2]+1);
 
-    res = dp[i];
+    ans = v[i];
   }
 
-  cout << res << endl;
+  cout << ans;
 
   return 0;
 }
