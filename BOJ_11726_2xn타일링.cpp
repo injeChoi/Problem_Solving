2cbc0413807e347b83477643d18e587c9976223c
injeChoi <2015jasonc@gmail.com>
2020-02-11 18:28:39 +0900

    백준 11726번, 2xn타일링

---
+++ BOJ_11726_2xn타일링.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int N; cin >> N;
+  vector<int> v(1001);
+
+  v[0] = 1;
+  v[1] = 1;
+  for (int i = 2; i <= N; i++)
+    v[i] = (v[i-1] + v[i-2]) % 10007;
+
+  cout << v[N] << "\n";
+  return 0;
+}
