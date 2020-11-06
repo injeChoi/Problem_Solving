dab80b5c2bfb1d1cfb81a005257b7549287dc282
injeChoi <2015jasonc@gmail.com>
2020-11-07 02:13:31 +0900

    백준 1662번, 압축

---
+++ BOJ_1662_압축.cpp
@@ -0,0 +1,53 @@
+/*
+https://www.acmicpc.net/problem/1662
+bottom-up으로, 재귀적으로 푸는게 관건.
+bracket 배열에 특정 괄호의 끝점을 표기해두면
+재귀 함수에서 괄호가 시작되는 부분이 발견될 때 
+그 괄호의 끝은 어딘지 알려줄 수 있게된다. 
+*/
+#include <iostream> 
+#include <string> 
+#include <stack> 
+using namespace std; 
+
+int bracket[50]; 
+string str;
+
+void init() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+int recursive(int start, int end) { 
+    int result = 0; 
+    int round = 0;
+    for (int i = start; i < end; i++) { 
+        if (str[i] == '(') { 
+            round = str[i - 1] - '0'; 
+            result += round * recursive(i + 1, bracket[i]) - 1; 
+            i = bracket[i];
+            continue; 
+        } 
+        result++; 
+    } 
+    return result; 
+} 
+
+int main(void) { 
+    init();
+    stack<int> s;
+    cin >> str; 
+    
+    for (int i = 0; i < str.length(); i++) { 
+        if (str[i] == '(') 
+            s.push(i); 
+        
+        else if (str[i] == ')') { 
+            bracket[s.top()] = i; 
+            s.pop(); 
+        } 
+    } 
+    cout << recursive(0, str.length()) << endl; 
+    return 0; 
+}
