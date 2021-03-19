f29eb60b5e199f166f38baabcf1a43179cebb960
injeChoi <2015jasonc@gmail.com>
2021-03-19 22:02:15 +0900

    백준 14725번, 개미굴(실패)

---
+++ BOJ_14725_개미굴.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N;
+vector<vector<string> > v;
+vector<string> element;
+
+void init() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+int main(int argc, char const *argv[]) {
+    init();
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        int num; cin >> num;
+        for (int j = 0; j < num; j++) {
+            string str; cin >> str;
+            element.push_back(str);
+        }
+        v.push_back(element);
+        element.clear();
+    }
+    sort(v.begin(), v.end());
+
+    for (int i = 0; i < v.size(); i++) {
+        string line = "";
+        for (int j = 0; j < v[i].size(); j++) {
+            if (i == 0) {
+                cout << line << v[i][j] << endl;
+                line += "--";
+                continue;
+            }
+            else {
+                if (v[i][j] == v[i-1][j]) {
+                    line += "--";
+                    continue;
+                }
+            }
+            cout << line << v[i][j] << endl;
+            line += "--";
+        }
+    }
+
+    return 0;
+}
