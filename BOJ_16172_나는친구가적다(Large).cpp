3953a1bd833c6b5a7eecde9c5db0c3530bce052b
injeChoi <2015jasonc@gmail.com>
2021-03-21 10:33:37 +0900

    백준 16172번, 나는 친구가 적다 (Large)

---
+++ BOJ_16172_나는친구가적다(Large).cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string tmp, input;
+vector<char> v;
+char filtered[200001], look[200001];
+
+void init() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+int main(int argc, char const *argv[]) {
+    init();
+    cin >> tmp >> look;
+
+    for (int i = 0; i < tmp.size(); i++) 
+        if (!isdigit(tmp[i])) 
+            input += tmp[i];
+
+    for (int i = 0; i < input.size(); i++) 
+        filtered[i] = input[i];
+    
+    char *ptr = strstr(filtered, look);
+
+    ptr != NULL ? cout << 1 << endl : cout << 0 << endl;
+    return 0;
+}
