528e3453ce4378e3e14d4e695691a7b7dbafd986
injeChoi <2015jasonc@gmail.com>
2021-06-26 17:15:25 +0900

    test

---
+++ Programmers/Level_1/test.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void init() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+int main(int argc, char const *argv[]) {
+    init();
+    //freopen("input.txt", "r", stdin);
+
+    return 0;
+}
