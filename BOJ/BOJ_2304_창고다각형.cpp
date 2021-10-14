24af622ce794e30192fb41488a7afcae580f020d
injeChoi <2015jasonc@gmail.com>
2021-10-14 23:11:44 +0900

    .

---
+++ BOJ/BOJ_2304_창고다각형.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> pii;
+
+int N, high, low, ans, idx = 0;
+vector<pii> v;
+
+void init() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+int main(int argc, char const *argv[]) {
+    init();
+    cin >> N;
+    
+    return 0;
+}
