aa05d54d3c70d4b573abf4f437b964980e48760e
injeChoi <2015jasonc@gmail.com>
2022-03-09 18:28:10 +0900

    .

---
+++ Programmers/Level_2/Swift_Code/Swift_Code/N개의최소공배수.swift
@@ -0,0 +1,32 @@
+//
+//  N개의최소공배수.swift
+//  Swift_Code
+//
+//  Created by InJe Choi on 2022/03/09.
+//
+
+import Foundation
+
+func gcd(_ a: Int, _ b: Int) -> Int {
+    if b == 0 {
+        return a
+    } else {
+        return gcd(b, a % b)
+    }
+}
+
+func lcm(_ a: Int, _ b: Int) -> Int {
+    return a * b / gcd(a, b)
+}
+
+func solution(_ arr:[Int]) -> Int {
+    var answer = 1
+
+    arr.forEach {
+        answer = lcm(answer, $0)
+    }
+
+    return answer
+}
+
+print(solution([1,2,3]))
