a7d44fe6842425aa6c161164143567bdd546adca
injeChoi <2015jasonc@gmail.com>
2022-03-22 13:13:14 +0900

    .

--- Programmers/Level_2/Swift_Code/Swift_Code/하노이의탑.swift
+++ Programmers/Level_2/Swift_Code/Swift_Code/하노이의탑.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+var answer = [[Int]]()
+
+func hanoi(_ n: Int, _ start: Int, _ mid: Int, _ end: Int) {
+    if n == 1 {
+        answer.append([start, end])
+        return
+    }
+    
+    hanoi(n-1, start, end, mid)
+    answer.append([start, end])
+    hanoi(n-1, mid, start, end)
+}
+
+func solution(_ n: Int) -> [[Int]] {
+    hanoi(n, 1, 2, 3)
+    return answer
+}
