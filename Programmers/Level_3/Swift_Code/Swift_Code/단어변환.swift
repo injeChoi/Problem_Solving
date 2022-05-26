f671c07af425d4fb8c83ec3213b6bd7cd3f91fa5
injeChoi <2015jasonc@gmail.com>
2022-05-26 10:27:04 +0900

    .

--- Programmers/Level_3/Swift_Code/Swift_Code/단어변환.swift
+++ Programmers/Level_3/Swift_Code/Swift_Code/단어변환.swift
@@ -8,6 +8,10 @@
 /*
  https://programmers.co.kr/learn/courses/30/lessons/43163?language=swift
  
+ 방문처리 배열 만든 후, words배열 안에 원소들이랑 한 글자 씩 바껴 간 begin(tmp)랑 비교
+ 방문하지 않은 원소이고 차이가 2 미만이면 그 단어로 바껴보고 다음 단어 탐색
+ 이런식으로 dfs 쭉 진행하다가 begin(tmp) == target인 경우 바뀐 횟수 답으로 처리
+ 이때, 바뀐 횟수가 답 보다 크면 return 
  */
 
 import Foundation
